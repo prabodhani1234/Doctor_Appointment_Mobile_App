818a130229b45e99df082e8abd9a28dae057751f
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-11-22 10:37:53 +0530

    adding fonts

---
+++ app/Root.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Root extends Component {
+  constructor(props){
+    super(props);
+    this.state={}
+  };
+
+
+  render() {
+    return(
+      <View>
+        
+          
+          <Text>
+          
+            Hellow</Text>
+        
+      </View>
+    );
+  }; 
+};
