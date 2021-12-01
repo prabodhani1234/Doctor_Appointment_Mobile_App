9abcc45a3c2edfbfbdca10b95f5da7603b217f69
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-02 01:29:14 +0530

    create home screen and set navigation

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';  
+import {  Text, View,} from 'react-native';
+
+export default class Appointment extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        )
+    }
+}
