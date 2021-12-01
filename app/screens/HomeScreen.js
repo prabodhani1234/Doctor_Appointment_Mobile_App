7dea45be49426f7800fe71e05fd376f526955e35
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-02 01:40:12 +0530

    add search bar for home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';  
-import {  Text, View,} from 'react-native';
+import {  Text, View, StyleSheet, TextInput, SafeAreaView, ScrollView} from 'react-native';
 
-export default class Appointment extends Component{
+export default class HomeScreen extends Component{
     render(){
         return(
-            <View>
-                <Text>Home Screen</Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+                <ScrollView  scrollEventThrottle={16}>
+                <View >
+                    <View style={styles.textInputContainer}>
+                        <TextInput 
+                        placeholder='Search Your Doctor'
+                        />
+                    </View>
+                </View >
+                </ScrollView>
+            </SafeAreaView>
         )
     }
-}
+};
+const styles= StyleSheet.create(
+    {
+        mainContainer:{ 
+            flex:1,
+            backgroundColor:'#3067F1',
+        },
+        textInputContainer:{
+            backgroundColor:'#ffffff',
+            marginLeft:"8%",
+            marginRight:"8%",
+            marginTop:"5%" ,
+            marginBottom:"5%",
+            borderRadius:6
+        },
+    }
+);
