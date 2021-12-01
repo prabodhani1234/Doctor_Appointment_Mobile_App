a8d0ddc117db2fb14461f6b3dcc4df2d153ed70c
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-02 01:07:57 +0530

    add image for forgotpassword screen

--- app/theme/Images.js
+++ app/theme/Images.js
@@ -1,6 +1,7 @@
 const Images={
     login: require('../assets/Images/login.png'),
     signup: require('../assets/Images/signup.png'),
+    forgotpassword: require('../assets/Images/forgot.png'),
 
     google : require('../assets/Images/Google.png'),
     facebook: require('../assets/Images/Facebook.png'),
