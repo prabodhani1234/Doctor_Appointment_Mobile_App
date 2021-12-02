b815e7743f1193a280ebf5ded98c6935ce16f647
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-03 00:38:37 +0530

    add profile image styles

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';  
-import { SafeAreaView, StyleSheet, Text, View,ScrollView, } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,ScrollView,Image } from 'react-native';
+import Images from '../theme/Images';
 
 export default class Profile extends Component{
     render(){
@@ -10,6 +11,11 @@ export default class Profile extends Component{
                         <View style={styles. mainTextContainer}>
                             <Text style={styles.mainText}>My Profile</Text>
                         </View>
+                        <View style={styles.imageView}>
+                        <View style={styles.imageContainer}>
+                        <Image source={Images.doctor1} style={styles.imageStyle}/>
+                        </View>
+                        </View>
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -18,19 +24,43 @@ export default class Profile extends Component{
 }
 const styles=StyleSheet.create(
     {
-      mainContainer:{ 
-        flex:1,
-        backgroundColor:'#3067F1',
-    },
-    mainTextContainer:{
-      flex:1,
-      marginBottom:"40%",
-      paddingTop:25
-    },
-    mainText:{
-      fontSize:26,
-      color:"#ffffff",
-      textAlign:'center'
-    }
+        mainContainer:{ 
+            flex:1,
+            backgroundColor:'#3067F1',
+        },
+        mainTextContainer:{
+            flex:1,
+            marginBottom:"40%",
+            paddingTop:25
+        },
+        mainText:{
+            fontSize:26,
+            color:"#ffffff",
+            textAlign:'center'
+        },
+        imageView:{
+            flex:1, 
+            backgroundColor:'white', 
+            width:'100%',
+            height:515, 
+            borderTopLeftRadius:20, 
+            borderTopRightRadius:20
+        }, 
+        imageContainer:{
+            width:170, 
+            height:170, 
+            borderRadius:170, 
+            borderColor:'#707070',
+            backgroundColor:'#E0E0E0', 
+            position:'absolute', 
+            left:'28%', 
+            top:-100
+        }, 
+        imageStyle:{
+            width:150, 
+            height:150,
+            left:10, 
+            top:10
+        }
     }
 )
