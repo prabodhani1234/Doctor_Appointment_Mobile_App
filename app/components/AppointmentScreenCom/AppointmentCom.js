660c41bdc29f15970c55fd0921c2385657e5e5e7
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-02 22:10:03 +0530

    create past appointment component

---
+++ app/components/AppointmentScreenCom/AppointmentCom.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity
+} from 'react-native';
+import {Card} from 'react-native-shadow-cards';
+
+
+class AppointmentCom extends Component{
+
+
+  render(){
+    return(
+      <Card style={{ width:345, height:132,marginRight:10, elevation:10,marginTop:10}}>
+          <View style={{marginLeft:10}}>
+              <Text style={{color:'black', fontSize: 18,fontFamily:'Roboto-Bold',marginBottom:10,marginTop:5}}>
+              {this.props.name}
+              </Text>
+              <Text style={{color:'black',fontFamily:'Roboto-Bold'}}>{this.props.catogory}</Text>
+              <Text style={{fontFamily:'Roboto-Bold'}}>{this.props.date}</Text>
+              
+              <TouchableOpacity style={{paddingTop:10}}>
+                <Text style={{color:'blue',textDecorationLine:'underline'}}>{this.props.button}</Text>
+              </TouchableOpacity>
+              
+          </View>
+          <View>
+            <View>
+                <Image source={this.props.imageUri} style={{ height:80, width:80 ,resizeMode:'contain', position:'absolute', left:220, bottom:28}}/>
+            </View>
+            <TouchableOpacity style={{backgroundColor:'#3067F1',marginLeft:177, marginRight:10, position:'absolute', bottom:0.1, padding:3,borderRadius:5, paddingLeft:10, paddingRight:10}}>
+                <Text style={{textAlign:'center', color:'#ffffff'}}>Change Reschedule</Text>
+            </TouchableOpacity>
+          </View>
+      </Card>
+    )
+  }
+}
+export default AppointmentCom;
--- app/screens/AppointmentScreens/PastAppointment.js
+++ app/screens/AppointmentScreens/PastAppointment.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';  
-import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity  } from 'react-native';
+import { SafeAreaView, StyleSheet, View,ScrollView, } from 'react-native';
+import AppointmentCom from '../../components/AppointmentScreenCom/AppointmentCom';
+import Images from '../../theme/Images'
 
-export default class Appointment extends Component{
+
+
+export default class PastAppointment extends Component{
+
     render(){
         return(
-            <View>
-                <Text>passt Screen</Text>
-            </View>
+            <SafeAreaView style={styles.mainConataner}>
+                <ScrollView>
+                    <View style={styles.listcontainer}>
+                        <ScrollView>
+                            <View>
+                        
+                                <AppointmentCom imageUri={Images.doctor1}
+                                name="DR. Amelia Grosh" date='Nov 24 2021 06.30 pm' catogory='Cardiologiats' button='Feedback'
+                                />
+                             </View>
+                        </ScrollView>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
+
+const styles=StyleSheet.create({
+    mainConataner:{
+        flex:1,
+        backgroundColor:'#FCFCFC',
+    },
+    
+    listcontainer: {
+        width: '100%',
+        padding: 16,
+        paddingTop: 5,
+        
+      },
+})
