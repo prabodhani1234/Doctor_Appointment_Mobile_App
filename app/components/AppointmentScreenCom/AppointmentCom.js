b7d6bb046a655378422dd2c5f20a8b507531da7d
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-02 23:37:08 +0530

    chnage styles in appointment component

--- app/components/AppointmentScreenCom/AppointmentCom.js
+++ app/components/AppointmentScreenCom/AppointmentCom.js
@@ -3,7 +3,8 @@ import {
   Text,
   View,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  StyleSheet
 } from 'react-native';
 import {Card} from 'react-native-shadow-cards';
 
@@ -13,25 +14,23 @@ class AppointmentCom extends Component{
 
   render(){
     return(
-      <Card style={{ width:345, height:132,marginRight:"5%", elevation:10,marginTop:10}}>
+      <Card style={styles.mainConataner}>
           <View style={{marginLeft:10}}>
-              <Text style={{color:'black', fontSize: 18,fontFamily:'Roboto-Bold',marginBottom:10,marginTop:5}}>
-              {this.props.name}
-              </Text>
-              <Text style={{color:'black',fontFamily:'Roboto-Bold'}}>{this.props.catogory}</Text>
-              <Text style={{fontFamily:'Roboto-Bold'}}>{this.props.date}</Text>
+              <Text style={styles.docNameText}>{this.props.name}</Text>
+              <Text style={styles.catNameText}>{this.props.catogory}</Text>
+              <Text style={styles.dateText}>{this.props.date}</Text>
               
               <TouchableOpacity style={{paddingTop:10}}>
-                <Text style={{color:'blue',textDecorationLine:'underline'}}>{this.props.button}</Text>
+                <Text style={styles.subButton}>{this.props.button}</Text>
               </TouchableOpacity>
               
           </View>
           <View>
             <View>
-                <Image source={this.props.imageUri} style={{ height:80, width:80 ,resizeMode:'contain', position:'absolute', left:220, bottom:28}}/>
+                <Image source={this.props.imageUri} style={styles.imagestyle}/>
             </View>
-            <TouchableOpacity style={{backgroundColor:'#3067F1',marginLeft:"55%", marginRight:10, position:'absolute', bottom:0.1, padding:3,borderRadius:5, paddingLeft:10, paddingRight:10}}>
-                <Text style={{textAlign:'center', color:'#ffffff'}}>{this.props.mainButton}</Text>
+            <TouchableOpacity style={styles.mainButtonContainer}>
+                <Text style={styles.mainButtonText}>{this.props.mainButton}</Text>
             </TouchableOpacity>
           </View>
       </Card>
@@ -42,15 +41,52 @@ export default AppointmentCom;
 
 const styles=StyleSheet.create({
     mainConataner:{
-        flex:1,
-        backgroundColor:'#FCFCFC',
+        width:345, 
+        height:132,
+        marginRight:"5%", 
+        elevation:10,
+        marginTop:'3%'
     },
     
-    listcontainer: {
-        width: '100%',
-        padding: 16,
-        paddingTop: 5,
-        height:'100%'
-        
-      },
+    docNameText:{
+        color:'black', 
+        fontSize: 18,
+        fontFamily:'Roboto-Bold',
+        marginBottom:"3%",
+        marginTop:'2%'
+    },
+    catNameText:{
+        color:'black',
+        fontFamily:'Roboto-Bold',
+    },
+    dateText:{
+        fontFamily:'Roboto-Bold',
+    },
+    subButton:{
+        color:'blue',
+        textDecorationLine:'underline'
+    },
+    imagestyle:{
+        height:80, 
+        width:80 ,
+        resizeMode:'contain', 
+        position:'absolute', 
+        left:225, 
+        bottom:28
+    },
+    mainButtonContainer:{
+        backgroundColor:'#3067F1',
+        marginLeft:"55%", 
+        marginRight:10, 
+        position:'absolute', 
+        bottom:0.1, 
+        padding:3,
+        borderRadius:5, 
+        paddingLeft:'3%', 
+        paddingRight:'3%'
+    },
+    mainButtonText:{
+        textAlign:'center', 
+        color:'#ffffff'
+    }
 })
