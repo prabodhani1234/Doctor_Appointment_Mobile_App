a1e904db8c1aa193d08af249fad3c3da13454c2f
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-03 00:03:54 +0530

    add main tittle for profile Screen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';  
-import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity  } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,ScrollView, } from 'react-native';
 
 export default class Profile extends Component{
     render(){
         return(
-            <View>
-                <Text>Profile Screen</Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+                <ScrollView >
+                    <View>
+                        <View style={styles. mainTextContainer}>
+                            <Text style={styles.mainText}>My Profile</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
+const styles=StyleSheet.create(
+    {
+      mainContainer:{ 
+        flex:1,
+        backgroundColor:'#3067F1',
+    },
+    mainTextContainer:{
+      flex:1,
+      marginBottom:"40%",
+      paddingTop:25
+    },
+    mainText:{
+      fontSize:26,
+      color:"#ffffff",
+      textAlign:'center'
+    }
+    }
+)
