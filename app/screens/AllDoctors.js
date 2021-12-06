db6773b791368556dfb9a041439c026c6801f399
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-06 20:17:05 +0530

    add tittle

--- app/screens/AllDoctors.js
+++ app/screens/AllDoctors.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';  
-import { Text, View, } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,ScrollView,TouchableOpacity  } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Appointment extends Component{
+    onPressHomeButton=()=>{
+        this.props.navigation.navigate('Home')
+      }
     render(){
         return(
-            <View>
-                <Text>All Doctors Screen</Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+                <ScrollView >
+                    <View>
+                        <View style={styles. mainTextContainer}>
+                            <Text style={styles.mainText}>Available Doctors</Text>
+                            
+                        </View>
+                        
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
-}
+};
+const styles=StyleSheet.create(
+    {
+        mainContainer:{ 
+            flex:1,
+            backgroundColor:'#3067F1',
+        },
+        mainTextContainer:{
+            flex:1,
+            marginBottom:"40%",
+            paddingTop:25
+        },
+        mainText:{
+            fontSize:26,
+            color:"#ffffff",
+            textAlign:'center',
+            position:'absolute',
+            left:'25%',
+            top:'60%'
+        },
+    }
+)
