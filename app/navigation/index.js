13ab804152bae5931a2c3b92f5e3f92c32e2c0f1
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-06 21:20:46 +0530

    router cover in authprovider

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
+import { AuthProvider } from './AuthProvider';
 import Routes from './Routes';
+
 const Provider =()=>{
   return(
+    <AuthProvider>
       <Routes/>
+    </AuthProvider>
+      
     
   )
 };
