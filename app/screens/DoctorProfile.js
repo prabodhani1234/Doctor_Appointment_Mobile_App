8f0c825a8cb79d4cdf4fe2212d4d3e45faab0dda
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-09 20:31:07 +0530

    coding for main view

--- app/screens/DoctorProfile.js
+++ app/screens/DoctorProfile.js
@@ -4,9 +4,32 @@ import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,Touch
 export default class Appointment extends Component{
     render(){
         return(
-            <View>
-                <Text>Doctor Profile</Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+                <ScrollView>
+                    
+                    <View style={styles.container}>
+                        
+                    </View>
+                    
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
+const styles= StyleSheet.create(
+    {
+        mainContainer:{
+            flex:1,
+            backgroundColor:'#3067F1',
+            alignItems:'center',
+            justifyContent:'center'
+        },
+        container:{
+            flex:1, 
+            backgroundColor:'#FCFCFC',
+            width:'100%',
+            alignItems:'center',
+            justifyContent:'center'
+        }
+    }
+)
