aa6a168c0a86a0b9964157be7e0e9e85182da19f
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-10 22:28:01 +0530

    add all profile buttons

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -2,14 +2,16 @@ import React, { useContext } from 'react';
 import { SafeAreaView, StyleSheet, Text, View,ScrollView,Image,TouchableOpacity } from 'react-native';
 import Images from '../theme/Images';
 import { AuthContext } from '../navigation/AuthProvider';
+import Icon  from 'react-native-vector-icons/FontAwesome5';
+
 
 const Profile=()=>{
     const { logout}=useContext(AuthContext);
 
         return(
-            <SafeAreaView style={styles.mainContainer}>
+            <SafeAreaView >
                 <ScrollView >
-                    <View>
+                    <View style={styles.mainContainer}>
                         <View style={styles. mainTextContainer}>
                             <Text style={styles.mainText}>My Profile</Text>
                         </View>
@@ -20,11 +22,42 @@ const Profile=()=>{
                             <View style={styles.textContaniner}>
                                 <Text style={styles.nameTextStyle}>Ashraf Hossain</Text>
                                 <Text>ashraf@gmail.com</Text>
-                                <TouchableOpacity onPress={()=> logout()}>
-                                    <Text style={{textAlign:'center'}}>Log Out</Text>
+                                
+                            </View>
+                            <View style={styles.manuContainer}>
+                                <TouchableOpacity onPress={() => {}} activeOpacity={0.5} >
+                                    <View style={styles.menuItem}>
+                                        <Icon name='credit-card' size={25} style={{paddingRight:6}}/>
+                                        <Text style={styles.menuItemText}>Payment</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => {}} activeOpacity={0.5}>
+                                    <View style={styles.menuItem}>
+                                        <Icon name='map-marker-alt' size={25} style={{paddingRight:15}} />
+                                        <Text style={styles.menuItemText}>Location</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => {}} activeOpacity={0.5}>
+                                    <View style={styles.menuItem}>
+                                        <Icon name='user-edit' size={25} style={{paddingRight:3}}/>
+                                        <Text style={styles.menuItemText}>Support</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => {}} activeOpacity={0.5}>
+                                    <View style={styles.menuItem}>
+                                        <Icon name='cog' size={25} style={{paddingRight:10}}/>
+                                        <Text style={styles.menuItemText}>Settings</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={()=> logout()} activeOpacity={0.5}>
+                                    <View style={styles.menuItem}>
+                                        <Icon name='sign-out-alt' size={25} style={{paddingRight:10}}/>
+                                        <Text style={styles.menuItemText}>Log Out</Text>
+                                    </View>
                                 </TouchableOpacity>
                             </View>
                         </View>
+                        
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -79,7 +112,23 @@ const styles=StyleSheet.create(
             fontFamily:'Roboto-Bold', 
             fontSize:19, 
             color:'black'
-        }
+        },
+        manuContainer:{
+            marginTop:'6%',
+            marginLeft:'10%'
+        },
+        menuItem: {
+            flexDirection: 'row',
+            paddingVertical: 15,
+           
+          },
+          menuItemText: {
+            color: '#777777',
+            marginLeft: 20,
+            fontWeight: '600',
+            fontSize: 16,
+            lineHeight: 26,
+          },
     }
 )
 export default Profile;
