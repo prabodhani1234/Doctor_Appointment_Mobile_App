00cf0d7cd9b8ad2069fdbf1e9c2de0cb01587361
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-11 19:33:22 +0530

    add image and name for edit profile screen

--- app/screens/EditProfileScreen.js
+++ app/screens/EditProfileScreen.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';  
-import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity,ImageBackground } from 'react-native';
+import { SafeAreaView,Button, Image, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity,ImageBackground } from 'react-native';
 import Images from '../theme/Images';
 
 
-export default class EditProfileScreen extends Component{
-    render(){
+const EditProfileScreen= () =>{
         return(
-            <SafeAreaView>
-                <View style={styles.mainContainer}>
-                    <TouchableOpacity>
-                        <View>
-                            <ImageBackground source={Images.doctor1}>
-
-                            </ImageBackground>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </SafeAreaView>
+            <View style={styles.mainContainer}>
+            <TouchableOpacity activeOpacity={0.8} >
+            <View
+              style={styles.imageContainer}>
+              <ImageBackground
+                source={Images.doctor1}
+                style={styles.imageStyle}
+                imageStyle={{borderRadius: 15}}>
+              </ImageBackground>
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.nameText}>
+            Johne
+          </Text>
+        </View>
+          
         )
-    }
+    
 }
 const styles= StyleSheet.create(
     {
@@ -26,6 +30,24 @@ const styles= StyleSheet.create(
             flex:1,
             backgroundColor:'#ffffff',
             alignItems:'center',
+        },
+        imageContainer:{
+            height: 100,
+            width: 100,
+            borderRadius: 15,
+            marginTop:50,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        imageStyle:{
+            height: 150, 
+            width: 150,
+        },
+        nameText:{
+            marginTop: 30, 
+            fontSize: 18, 
+            fontWeight: 'bold',
         }
     }
 )
+export default EditProfileScreen;
