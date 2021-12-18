dba0cc4212271e6f146b133db09a35518a316275
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-18 22:42:06 +0530

    create appointment screen ui

--- app/screens/AppointmentBookingScreen.js
+++ app/screens/AppointmentBookingScreen.js
@@ -1,12 +1,102 @@
 import React, { Component } from 'react';  
 import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity  } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Appointment extends Component{
     render(){
         return(
-            <View>
-                <Text>3Screen</Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+               
+                    <View style={styles.container}>
+                    <ScrollView>
+                        <Text style={styles.textStyle}>Full Name</Text>
+                        <TextInput
+                            placeholder="Enter your name"
+                            style={styles.inputStyle}
+                        />
+                        <Text style={styles.textStyle}>Phone Number</Text>
+                        <TextInput
+                            placeholder="Enter Phone Number"
+                            style={styles.inputStyle}
+                        />
+                        <Text style={styles.textStyle}>E-mail Address</Text>
+                        <TextInput
+                            placeholder="Enter Email"
+                            style={styles.inputStyle}
+                        />
+                        <Text style={styles.textStyle}>Address</Text>
+                        <TextInput
+                            placeholder="Enter your Address"
+                            style={styles.inputStyle}
+                        />
+                        <Text style={styles.textStyle}>Date</Text>
+                        <View style={styles.inputStyle}>
+
+                            <TextInput
+                                placeholder="Select Date"
+                                
+                            />
+                            <Icon name='user' size={25} style={{ marginLeft:'55%', marginTop:'4%'}}/>
+                        </View>
+                        
+                        <Text style={styles.textStyle}>Time</Text>
+                        <TextInput
+                            placeholder="Select Time"
+                            style={styles.inputStyle}
+                        />
+                        <View style={styles.buttonContainer}>
+                            <Text style={styles.buttonText}>Conform</Text>
+                        </View>
+                        </ScrollView>
+                    </View>
+                    
+                
+            </SafeAreaView>
         )
     }
 }
+const styles=StyleSheet.create(
+    {
+        mainContainer:{
+            flex:1, 
+            backgroundColor:'#3067F1'
+        },
+        container:{
+            flex:1,
+            backgroundColor:'#FCFCFC', 
+            paddingLeft:'10%', 
+            marginTop:'10%', 
+            borderTopLeftRadius:20, 
+            borderTopRightRadius:20,
+            paddingTop:30
+        },
+        textStyle:{
+            fontSize:16,
+            color:'black'
+        },
+        inputStyle:{
+            backgroundColor:'#ffffff',
+            width:'90%', 
+            marginTop:'2%', 
+            borderColor:'#707070', 
+            borderWidth:1, 
+            marginBottom:'2%', 
+            paddingLeft:20,
+            flexDirection:'row'
+        },
+        buttonContainer:{
+            alignItems:'center', 
+            backgroundColor:'#3B68E7',
+            marginRight:'20%', 
+            marginLeft:'10%', 
+            marginTop:'4%', 
+            paddingTop:8, 
+            paddingBottom:8,
+            borderRadius:15
+        },
+        buttonText:{
+            color:'#ffffff',
+            fontSize:20
+        }
+    }
+)
