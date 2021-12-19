d79bd562571d01bc104550b485f97169ae51b36e
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-19 10:58:24 +0530

    add credit card details styles

--- app/screens/PaymentScreen.js
+++ app/screens/PaymentScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';  
-import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity  } from 'react-native';
+import { SafeAreaView,Button, StyleSheet, Text, TextInput, View,ScrollView,TouchableOpacity,Image  } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Images from '../theme/Images';
 
 export default class PaymentScreen extends Component{
     render(){
@@ -9,10 +10,23 @@ export default class PaymentScreen extends Component{
                 <View style={styles.container}>
                     <ScrollView>
                         <View style={{width:'92%', height:180, backgroundColor:'#3067F1', borderRadius:20}}>
-                            <View>
-                               
+                            <View style={{marginLeft:'6%'}}>
+                                <View style={styles.textContainer}>
+                                <Text style={styles.card1fontstyle}>Credit Card</Text>
+                                <Text style={styles.card2fontstyle}>Central Bank</Text>
+                                </View>
+                                <View style={styles.textContainer}>
+                                    <Text>Image</Text>
+                                    <Text style={styles.cardtextStyle}>Sri Lanka</Text>
+                                </View>
+                                <View style={{marginTop:'15%', flexDirection:'row'}}>
+                                    <Text style={styles.card1fontstyle}>1234 5678 9458 2587</Text>
+                                    <Text style={{color:'#ffffff',fontFamily:'Roboto-Bold',fontSize:16,marginLeft:'19%'}}>Expair</Text>
+                                </View>
                             </View>
                         </View>
+                        
+                        
                     </ScrollView>
                 </View>
             </SafeAreaView>
@@ -33,6 +47,45 @@ const styles=StyleSheet.create(
             borderTopLeftRadius:20, 
             borderTopRightRadius:20,
             paddingTop:30
+        },
+        textContainer:{
+            flexDirection:'row',
+            marginTop:8
+        },
+        card1fontstyle:{
+            color:'#ffffff',
+            fontFamily:'Roboto-Bold',
+            fontSize:16,
+        },
+        card2fontstyle:{
+            color:'#ffffff',
+            fontFamily:'Roboto-Bold',
+            fontSize:16,
+            marginLeft:'28.5%'
+        },
+        imageStyles:{
+            width:100,
+            height:100
+        },
+        cardtextStyle:{
+            color:'#ffffff',
+            fontFamily:'Roboto-Bold',
+            fontSize:13,
+            marginLeft:'60.5%'
+        },
+        textStyle:{
+            fontSize:16,
+            color:'black'
+        },
+        inputStyle:{
+            backgroundColor:'#ffffff',
+            width:'90%', 
+            marginTop:'2%', 
+            borderColor:'#707070', 
+            borderWidth:1, 
+            marginBottom:'2%', 
+            paddingLeft:20,
+            flexDirection:'row'
         },
     }
 )
