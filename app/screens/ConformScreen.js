121067277d4ab0e71601f9650751164e079f36bc
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-12-19 22:55:41 +0530

    change position of style in conform screen

--- app/screens/ConformScreen.js
+++ app/screens/ConformScreen.js
@@ -5,13 +5,13 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 export default class Conform extends Component{
     render(){
         return(
-            <SafeAreaView style={{flex:1, backgroundColor:'#ffffff'}}>
-                <View style={{flex:1,alignItems:'center', justifyContent:'center'}}>
+            <SafeAreaView style={styles.mainContainer}>
+                <View style={styles.container}>
                     <View>
                         <Icon name='check-circle' size={150} style={{color:'#3067F1'}}/>   
                     </View>
-                    <Text style={{fontSize:22, fontFamily:'Roboto-Bold',color:'black'}}>Your Appointment had </Text>
-                    <Text style={{fontSize:22, fontFamily:'Roboto-Bold',color:'black'}}>been Scheduled</Text>
+                    <Text style={styles.textStyle}>Your Appointment had </Text>
+                    <Text style={styles.textStyle}>been Scheduled</Text>
                 </View>
                 <TouchableOpacity style={styles.buttonContainer} activeOpacity={0.8} >
                     <Text style={styles.buttonText}>Ok</Text>
@@ -22,6 +22,20 @@ export default class Conform extends Component{
 }
 const styles=StyleSheet.create(
     {
+        mainContainer:{
+            flex:1, 
+            backgroundColor:'#ffffff',
+        },
+        container:{
+            flex:1,
+            alignItems:'center', 
+            justifyContent:'center'
+        },
+        textStyle:{
+            fontSize:22, 
+            fontFamily:'Roboto-Bold',
+            color:'black'
+        },
         buttonContainer:{
             alignItems:'center', 
             backgroundColor:'#3B68E7',
